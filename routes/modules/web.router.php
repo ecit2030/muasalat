075ab82042d78012f8f35809b6de56005303d4b6
abdulazizoudah <azoz99aoda@hotmail.com>
2025-12-17 11:13:21 +0300

    Update Composer and remove ide-helper

--- routes/modules/web.router.php
+++ routes/modules/web.router.php
@@ -89,25 +89,7 @@ Route::get('wasl-package',function(Request $request){
     return $rejections;
 });
 
-function extractRejectionReasons($data): array
-{
-    $rejectionReasons = [];
-    // Check if the current data is an array
-    if (is_array($data) || is_object($data)) {
-        // Iterate over each item in the array or each property in the object
-        foreach ($data as $key => $value) {
-            // Check if the current key is 'rejectionReasons'
-            if ($key === 'rejectionReasons') {
-                // If so, add the value to the $rejectionReasons array
-                $rejectionReasons = array_merge($rejectionReasons, $value);
-            } else {
-                // Recursively call the function for each item or property
-                $rejectionReasons = array_merge($rejectionReasons, extractRejectionReasons($value));
-            }
-        }
-    }
-    return $rejectionReasons;
-}
+
 
 Route::get('sync',function(Request $request){
     \App\Models\User::whereId(1)->first()->syncRoles(['super','mainadmin','admin']);
