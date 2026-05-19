112c0c067a141c73c2270f254837b375fdba50b5
naylabengaied <naylabengaied@gmail.com>
2026-05-19 09:56:24 +0100

    update navbar landing

--- resources/views/Site/layouts/navbar.blade.php
+++ resources/views/Site/layouts/navbar.blade.php
@@ -18,7 +18,7 @@
             <li class="nav-item active">
 
                 @php
-                    $staticpages = Modules\StaticPage\Entities\StaticPage::all();
+                    $staticpages = StaticPage::orderBy('id', 'asc')->take(3)->get();
                 @endphp
 
                 @foreach ($staticpages as $staticpage)
